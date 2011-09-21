5c965ec57588de47a058eadca9e09bbd3325d1f8
Richard Jensen <facetnated@gmail.com>
2011-09-21 18:27:05 -0500

    First commit

---
+++ src/test/java/com/rhjensen/encryption/JCEExampleTest.java
@@ -0,0 +1,15 @@
+package com.rhjensen.encryption;
+
+import org.junit.Test;
+
+/**
+ * User: Richard H. Jensen
+ * Date: 9/21/11
+ * Time: 6:08 PM
+ */
+public class JCEExampleTest {
+    @Test
+    public void shouldGetSamePlaintextAfterEncryptingAndDecrypting() {
+
+    }
+}
