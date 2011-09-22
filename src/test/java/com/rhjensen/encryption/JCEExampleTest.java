d81890aa4c9aa79e04d0ab41d583610a154173e4
Richard Jensen <facetnated@gmail.com>
2011-09-21 20:21:30 -0500

    Adding the example code from http://stackoverflow.com/questions/587357/rijndael-support-in-java to my encrypt function. Adding try/catch to satisfy exception requirements.

--- src/test/java/com/rhjensen/encryption/JCEExampleTest.java
+++ src/test/java/com/rhjensen/encryption/JCEExampleTest.java
@@ -2,6 +2,17 @@ package com.rhjensen.encryption;
 
 import org.junit.Test;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,6 +31,30 @@ public class JCEExampleTest {
     }
 
     private String encrypt(String plainText) {
-        return plainText;
+        byte[] sessionKey = null; //Where you get this from is beyond the scope of this post
+        byte[] iv = null; //Ditto
+        byte[] ciphertext = null;
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            //You can use ENCRYPT_MODE or DECRYPT_MODE
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(sessionKey, "AES"), new IvParameterSpec(iv));
+
+            ciphertext = cipher.doFinal(plainText.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return ciphertext.toString();
+
     }
 }
