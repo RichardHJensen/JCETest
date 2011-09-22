61bf4010a82ea85375940efd7777f9f18db71129
Richard Jensen <facetnated@gmail.com>
2011-09-21 19:01:54 -0500

    Making the simplest test compile (but not run)

--- src/test/java/com/rhjensen/encryption/JCEExampleTest.java
+++ src/test/java/com/rhjensen/encryption/JCEExampleTest.java
@@ -2,6 +2,8 @@ package com.rhjensen.encryption;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * User: Richard H. Jensen
  * Date: 9/21/11
@@ -11,6 +13,13 @@ public class JCEExampleTest {
     @Test
     public void decryptingCiphertextShouldReturnOriginalPlaintext() {
         assertEquals("Original Plaintext", decrypt(encrypt("Original Plaintext")));
+    }
 
+    private String decrypt(String cipherText) {
+        return null;
+    }
+
+    private String encrypt(String plainText) {
+        return null;
     }
 }
